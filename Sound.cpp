a5e3ea5cbadc4df5bed7743732a953cb78f416ee
gui <guircp@usp.br>
2021-12-09 22:22:58 -0300

    inicio implementacao thread audio

--- Sound.cpp
+++ Sound.cpp
@@ -22,25 +22,24 @@ void audio_callback(void *userdata, Uint8 *stream, int len) {
 
 void Sound::play_async() {
 	SDL_AudioSpec want, have;
-	SDL_AudioDeviceID dev;
 
 	SDL_memset(&want, 0, sizeof(want)); /* or SDL_zero(want) */
 	want.freq = 48000;
 	want.format = AUDIO_F32;
 	want.channels = 2;
 	want.samples = 4096;
-	want.callback = audio_callback; 
+	want.callback = MyAudioCallback; /* you wrote this function elsewhere -- see SDL_AudioSpec for details */
 
-	dev = SDL_OpenAudioDevice(NULL, 0, &want, &have, SDL_AUDIO_ALLOW_FORMAT_CHANGE);
-	if (dev == 0) {
+	if (SDL_OpenAudio(&want, &have) < 0) {
 	    SDL_Log("Failed to open audio: %s", SDL_GetError());
+	} else {
+	    if (have.format != want.format) {
+	        SDL_Log("We didn't get Float32 audio format.");
+	    }
+	    SDL_PauseAudio(0); /* start audio playing. */
+	    SDL_Delay(5000); /* let the audio callback play some sound for 5 seconds. */
+	    SDL_CloseAudio();
 	}
-	else if (have.format != want.format)  /* we let this one thing change. */
-	    SDL_Log("We didn't get Float32 audio format.");
-	    
-	SDL_PauseAudioDevice(dev, 0); /* start audio playing. */
-	SDL_Delay(5000); /* let the audio callback play some sound for 5 seconds. */
-	SDL_CloseAudioDevice(dev);
 }
 
 
